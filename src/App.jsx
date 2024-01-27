50481fb1732bfcef9735846802cdd4e67c3fc19e
Eduardo Molina <eduardo.molina.me@hotmail.com>
2024-01-27 15:15:33 -0300

    Se declararon funciones en App.jsx para tener control de los estados desde el componente padre y se reubico el archivo Base.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import BaseColaboradores from './components/Base.js'
+import BaseColaboradores from './Base.js'
 import Buscador from './components/Buscador.jsx'
 import Formulario from './components/Formulario.jsx'
 import Listado from './components/Listado.jsx'
@@ -8,7 +8,33 @@ import { useState } from 'react'
 
 const App = () => {
   const [data, setData] = useState(BaseColaboradores)
-  const [dataFilter, setDataFilter] = useState(data)
+  const [dataFilter, setDataFilter] = useState('')
+  const [alert, setAlert]=useState({
+    msg:'',
+    color:''
+  })
+  const nuevosColaboradoresConId = (nuevoColaboradorConId)=>{
+    const ultimoValor = data[data.length-1]
+    const colaboradorConId = {...nuevoColaboradorConId, id: ultimoValor? Number(ultimoValor.id)+1:1}
+    setData([...datos, colaboradorConId])
+  }
+  const mostrarAlerta = (mensajeAlerta)=>{
+    setAlert(mensajeAlerta)
+  }
+  const sacarId = (idColaborador)=>{
+    const newList = data.filter((colaborador)=>colaborador.id != idColaborador)
+    setData(newList)
+  }
+  const filtro = data.filter((colaborador)=>{
+    return(
+      colaborador.nombre.toLowerCase().includes(dataFilter.toLowerCase()) ||
+      colaborador.correo.toLowerCase().includes(dataFilter.toLowerCase()) ||
+      colaborador.edad.toLowerCase().includes(dataFilter.toLowerCase()) ||
+      colaborador.cargo.toLowerCase().includes(dataFilter.toLowerCase()) ||
+      colaborador.telefono.toLowerCase().includes(dataFilter.toLowerCase())
+    )
+  })
+
   return (
     <>
       <h1>Lista de usuarios</h1>
